d7a3aa62a86a6a5213a15266647c7420b4937fc7
Balakumar3018 <balakumarjkp@gmail.com>
2022-04-03 14:59:27 +0530

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,8 +1,8 @@
-import LandingPage from "./landing-page/landing-page";
-import CartPage from "./cart-page/cart-page";
-import LoginPage from "./login-page/login-page";
-import LogoutPage from "./logout-page/logout-page";
-import ProductsPage from "./products-page/product-page";
-import WishlistPage from "./wishlist-page/wishlist";
+import LandingPage from "../../pages/landing-page/landing-page";
+import CartPage from "../../pages/cart-page/cart-page";
+import LoginPage from "../../pages/login-page/login-page";
+import LogoutPage from "../../pages/logout-page/logout-page";
+import ProductsPage from "../../pages/products-page/product-page";
+import WishlistPage from "../../pages/wishlist-page/wishlist";
 
 export {LandingPage,CartPage,LoginPage,LogoutPage,ProductsPage,WishlistPage};
